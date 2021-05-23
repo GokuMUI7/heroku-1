139d512a740c805f6022d59b037a159c6778d015
GokuMUI7 <39430301+GokuMUI7@users.noreply.github.com>
2021-05-24 02:04:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-wget https://github.com/libDrive/libDrive/releases/download/v1.3.1/libDrive.Server.v1.3.1.tar
-tar -xvf libDrive.Server.v1.3.1.tar
+wget https://github.com/libDrive/libDrive/releases/download/v1.3.2/libDrive.Server.v1.3.2.tar
+tar -xvf libDrive.Server.v1.3.2.tar
 pip3 install -r requirements.txt -q --no-cache-dir
 
 gunicorn main:app
