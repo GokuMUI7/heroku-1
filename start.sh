6c1aba907a90ba5119579572197044dd78701b37
GokuMUI7 <39430301+GokuMUI7@users.noreply.github.com>
2021-05-24 10:59:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+cd /usr/src/app
 wget https://github.com/libDrive/libDrive/releases/download/v1.3.2/libDrive.Server.v1.3.2.tar
 tar -xvf libDrive.Server.v1.3.2.tar
 pip3 install -r requirements.txt -q --no-cache-dir
